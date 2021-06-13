2bd476089677b45225b4defa14293fb2f4cfe6bd
Denis Zenevich <dzenevich@mail.ru>
2021-06-13 18:17:11 +0300

    Restore project to default state to add Refit support

---
+++ WEBApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WEBApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ WEBService/Controllers/CurrentTransformerGet.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WEBService.Models;
+
+namespace WEBService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CurrentTransformerGet : ControllerBase
+    {
+        WebServiceDBContext db;
+
+        public CurrentTransformerGet(WebServiceDBContext context)
+        {
+            db = context;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IEnumerable<CurrentTransformer>> Get(int id)
+        {
+            PointOfUse pou = await db.PointsOfUse.FirstOrDefaultAsync(x => x.PointOfUseId == id);
+            if (pou == null)
+            {
+                return null;
+            }
+
+            var outdated = from t1 in db.CurrentTransformers
+                           join t2 in db.ElectricityMeasuringPoints
+                           on t1.ElectricityMeasuringPointId equals t2.ElectricityMeasuringPointId
+                           join t3 in db.PointsOfUse on t2.PointOfUseId equals t3.PointOfUseId
+                           where t1.CheckDate < DateTime.Now && t3.PointOfUseId == id
+                           select t1;
+
+            return outdated;
+        }
+    }
+}

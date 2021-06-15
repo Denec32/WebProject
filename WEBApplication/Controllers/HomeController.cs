bd48dbe62e6ce833a64110b6372989f7cf220437
Denis Zenevich <dzenevich@mail.ru>
2021-06-15 11:54:48 +0300

    Add refit for WebAPI

--- WEBApplication/Controllers/HomeController.cs
+++ WEBApplication/Controllers/HomeController.cs
@@ -6,16 +6,21 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using WEBApplication.Models;
+using WEBService.Models;
 
 namespace WEBApplication.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IWEBServiceAPI _serviceAPI;
 
-        public HomeController(ILogger<HomeController> logger)
+
+ 
+        public HomeController(ILogger<HomeController> logger, IWEBServiceAPI api)
         {
             _logger = logger;
+            _serviceAPI = api;
         }
 
         public IActionResult Index()
@@ -33,5 +38,26 @@ namespace WEBApplication.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public async Task<IEnumerable<CurrentTransformer>> GetCurrentTransformer(int id)
+        {
+            return await _serviceAPI.GetCurrentTransformer(id);
+        }
+
+        public async Task<IEnumerable<ElectricityMeter>> GetElectricityMeter(int id)
+        {
+            return await _serviceAPI.GetElectricityMeter(id);
+        }
+
+        public async Task<IEnumerable<PotentialTransformer>>GetPotentialTransformer (int id)
+        {
+            return await _serviceAPI.GetPotentialTransformer(id);
+        }
+
+        public async Task<ActionResult<MeasuringPointCombined>> PostMeasuringPointCombined(MeasuringPointCombined measuringPointCombined)
+        {
+            return await _serviceAPI.PostMeasuringPointCombined(measuringPointCombined);
+        }
     }
+
 }
---
+++ WEBApplication/IWEBServiceAPI.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+using WEBService.Models;
+
+namespace WEBApplication
+{
+    public interface IWEBServiceAPI
+    {
+
+        [Get("/api/CurrentTransformerGet/{id}")]
+        Task<IEnumerable<CurrentTransformer>> GetCurrentTransformer(int id);
+
+        [Get("/api/ElectricityMeterGet/{id}")]
+        Task<IEnumerable<ElectricityMeter>> GetElectricityMeter(int id);
+
+        [Get("/api/PotentialTransformerGet/{id}")]
+        Task<IEnumerable<PotentialTransformer>> GetPotentialTransformer(int id);
+
+        [Post("/api/MeasuringPointInfo")]
+        Task<ActionResult<MeasuringPointCombined>> PostMeasuringPointCombined(MeasuringPointCombined measuringPointCombined);
+
+    }
+}

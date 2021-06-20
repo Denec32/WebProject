cd344f4d0d32fa454d1ab845ff694b23f9435d17
Denis Zenevich <dzenevich@mail.ru>
2021-06-20 23:37:02 +0300

    Fix fill form button

--- WEBApplication/wwwroot/js/site.js
+++ WEBApplication/wwwroot/js/site.js
@@ -1,17 +1,17 @@
 ﻿function FillForm() {
-    document.getElementById("ElectricityMeasuringPoint.Name").value = "Point 1";
+    document.getElementById("ElectricityMeasuringPoint_Name").value = "Point 1";
 
-    document.getElementById("ElectricityMeter.Number").value = "1A";
-    document.getElementById("ElectricityMeter.MeterType").value = "Type-01";
-    document.getElementById("ElectricityMeter.CheckDate").value = "2021-06-18T14:47";
+    document.getElementById("ElectricityMeter_Number").value = "1A";
+    document.getElementById("ElectricityMeter_MeterType").value = "Type-01";
+    document.getElementById("ElectricityMeter_CheckDate").value = "2021-06-18T14:47";
 
-    document.getElementById("CurrentTransformer.Number").value = "1B";
-    document.getElementById("CurrentTransformer.TransformerType").value = "Type-02";
-    document.getElementById("CurrentTransformer.TransformationRatio").value = 1.8;
-    document.getElementById("CurrentTransformer.CheckDate").value = "2021-06-18T14:47";
+    document.getElementById("CurrentTransformer_Number").value = "1B";
+    document.getElementById("CurrentTransformer_TransformerType").value = "Type-02";
+    document.getElementById("CurrentTransformer_TransformationRatio").value = 1.8;
+    document.getElementById("CurrentTransformer_CheckDate").value = "2021-06-18T14:47";
 
-    document.getElementById("PotentialTransformer.Number").value = "1C";
-    document.getElementById("PotentialTransformer.TransformerType").value = "Type-03";
-    document.getElementById("PotentialTransformer.TransformationRatio").value = 1.6;
-    document.getElementById("PotentialTransformer.CheckDate").value = "2021-06-18T14:47";
+    document.getElementById("PotentialTransformer_Number").value = "1C";
+    document.getElementById("PotentialTransformer_TransformerType").value = "Type-03";
+    document.getElementById("PotentialTransformer_TransformationRatio").value = 1.6;
+    document.getElementById("PotentialTransformer_CheckDate").value = "2021-06-18T14:47";
 }

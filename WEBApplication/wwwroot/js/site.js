517e291766f1c085919cb994680938f80ddfd76a
Denis Zenevich <dzenevich@mail.ru>
2021-06-21 12:38:53 +0300

    Fix initial ratios in fill form

--- WEBApplication/wwwroot/js/site.js
+++ WEBApplication/wwwroot/js/site.js
@@ -7,11 +7,11 @@
 
     document.getElementById("CurrentTransformer_Number").value = "1B";
     document.getElementById("CurrentTransformer_TransformerType").value = "Type-02";
-    document.getElementById("CurrentTransformer_TransformationRatio").value = 1.8;
+    document.getElementById("CurrentTransformer_TransformationRatio").value = "1,8";
     document.getElementById("CurrentTransformer_CheckDate").value = "2021-06-18T14:47";
 
     document.getElementById("PotentialTransformer_Number").value = "1C";
     document.getElementById("PotentialTransformer_TransformerType").value = "Type-03";
-    document.getElementById("PotentialTransformer_TransformationRatio").value = 1.6;
+    document.getElementById("PotentialTransformer_TransformationRatio").value = "1,6";
     document.getElementById("PotentialTransformer_CheckDate").value = "2021-06-18T14:47";
 }
